14b71fdefc1ab6c3dca8c98fb408554ee826746b
Yuri-k-156 <kanamaru.gf@gmail.com>
2020-05-04 16:32:40 +0900

    apply material-ui to events_new and events_show component

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Field, reduxForm } from 'redux-form';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField'
 
 import { getEvent, deleteEvent, putEvent } from '../actions';
 
@@ -16,10 +18,14 @@ class EventsShow extends Component {
 	renderField(field){
            const { input, label, type, meta: {touched, error} } = field
 	   return (
-		   <div>
-		     <input {...input} placeholder={label} type={type} />
-                     { touched && error && <span>{error}</span> }
-		   </div>
+		   <TextField
+		    hintText={label}
+		    floatingLabelText={label}
+		    type={type}
+		    errorText={touched && error}
+		    {...input}
+		    fulWidth={true}
+		   />
            )
 	}
 
@@ -43,16 +49,15 @@ class EventsShow extends Component {
 	render(){
 
 	  const { handleSubmit, pristine, submitting, invalid } = this.props
+          const style = { margin: 12 }
 
 	  return (
-		  <form onSubmit={handleSubmit(this.onSubmit)}>
+		  <form onSubmit={handleSubmit(this.onSubmit)}> 
 		    <div><Field label="Title" name="title" type="text" component={this.renderField} /></div>
 		    <div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
-		    <div>
-                      <input type="submit" values="Submit" disabled={pristine || submitting || invalid } />
-		      <Link to="/" >Cancel</Link>
-		      <Link to="/" onClick={this.onDeleteClick}>Delete</Link>
-		    </div>
+		  <RaisedButton label="Submit" type="Submit" style={style} disabled={pristine || submitting || invalid} />
+		    <RaisedButton label="Cancel" style={style} containerElement={<Link to="/" />} />
+		    <RaisedButton label="Delete" style={style} onClick={this.onDeleteClick} />
 		  </form>
 	  );
 	}
